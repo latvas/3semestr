89f4ad0fdd7d2069be2c0832e1f2c4d6fe88fb22
pavel_radist <latpavelvas@gmail.com>
2020-09-27 18:26:18 +0300

    added cd and execvp

--- microsha/main.cpp
+++ microsha/main.cpp
@@ -5,6 +5,8 @@
 #include <cstdlib>
 #include <cstring>
 #include <unistd.h>
+#include <sys/wait.h>
+#include <csignal>
 
 
 std::vector<std::string> split(const std::string &str) {
@@ -39,9 +41,62 @@ auto getCurrentDirectory() {
     return path_string;
 }
 
+int cd(const std::string& destinationDir) {     //returns 0 if everything is OK, 1 if error
+    int res;
+    if (destinationDir[0] == '/'){      //absolute path
+        res = chdir(destinationDir.c_str());
+    } else {                            //local path
+        std::string absDir = getCurrentDirectory() + "/" + destinationDir;
+        res = chdir(absDir.c_str());
+    }
+    if (res == -1){
+        fprintf(stderr, "cd: %s: Error when changing directory\n", destinationDir.c_str());
+    }
+    return res;
+}
+
+auto callExternalCommand(const std::vector<std::string>& data){
+    pid_t pid = fork();
+    if (pid == 0){ //child process
+        //printf("HC: hello from child\n");
+        std::vector<char *> v;
+        v.reserve(data.size());
+        for (const auto& word: data){
+            v.push_back((char *)word.c_str());
+        }
+        v.push_back(nullptr);
+        int res = execvp(v[0], &v[0]);
+        fprintf(stderr, "%s: command not found\n", data[0].c_str());
+        exit(1);
+    } else { //parent process
+        //printf("HP: hello from parent\n");
+        wait(nullptr);
+        //printf("CT: child has terminated\n");
+    }
+}
+
+int commandExecutor(const std::vector<std::string> &data) { // call command by name and arguments
+    if (data[0] == "cd") {   //first check if the command is internal
+        if (data.size() > 2) {
+            fprintf(stderr, "error: too many arguments for cd\n");
+            return 1;
+        }
+        return cd(data[1]);
+    } else if (data[0] == "pwd") {
+        printf("%s\n", getCurrentDirectory().c_str());
+        return 0;
+    } else if (data[0] == "time") {
+        printf("time\n"); // TODO Time command
+        return 0;
+    } else {    // call external command
+        callExternalCommand(data);
+    }
+    return 0;
+}
+
 void printWelcomeMessage(bool root) {
 
-    std::printf("[USERNAME %s]", getCurrentDirectory().c_str());
+    std::printf("[Pavlos %s]", getCurrentDirectory().c_str());
     if (root) {
         std::printf("! ");
     } else {
@@ -49,14 +104,15 @@ void printWelcomeMessage(bool root) {
     }
 }
 
-int shellLoop() {
+
+[[noreturn]] int shellLoop() {
     while (true) {
         std::string inputBuffer;
         printWelcomeMessage(false);
         std::getline(std::cin, inputBuffer);
         std::vector<std::string> words = split(inputBuffer); //split input string by space and tab
-        for (const auto &word: words) printf("%s\n", word.c_str()); //print parsed data
-        return 0;
+        //for (const auto &word: words) printf("%s\n", word.c_str()); //print parsed data
+        commandExecutor(words);
     }
 }
 
