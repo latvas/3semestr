2745fbb7deec82b299e7ebd9ad425c560a1d49ed
pavel_radist <latpavelvas@gmail.com>
2020-09-27 04:50:46 +0300

    showing working directory

--- microsha/main.cpp
+++ microsha/main.cpp
@@ -4,19 +4,19 @@
 #include <dirent.h>
 #include <cstdlib>
 #include <cstring>
-
+#include <unistd.h>
 
 
 std::vector<std::string> split(const std::string &str) {
     std::vector<std::string> words;
     bool isPrevSpace = true;
-    for (char i : str){
-        if (i == '>' || i == '<' || i == '|'){
+    for (char i : str) {
+        if (i == '>' || i == '<' || i == '|') {
             words.emplace_back(1, i);
             isPrevSpace = true;
             continue;
         }
-        if (!isspace(i)){
+        if (!isspace(i)) {
             if (isPrevSpace) {
                 words.emplace_back(); // add empty string
             }
@@ -31,29 +31,36 @@ std::vector<std::string> split(const std::string &str) {
     return words;
 }
 
+auto getCurrentDirectory() {
+    char *path = (char *) malloc(PATH_MAX * sizeof(char)); //allocating memory
+    char *res = getcwd(path, PATH_MAX);
+    std::string path_string(path);
+    free(path);
+    return path_string;
+}
 
-void printWelcomeMessage(std::string path, bool root){
+void printWelcomeMessage(bool root) {
 
-    std::printf("[USERNAME %s]", path.c_str());
-    if (root){
+    std::printf("[USERNAME %s]", getCurrentDirectory().c_str());
+    if (root) {
         std::printf("! ");
     } else {
         std::printf("> ");
     }
 }
 
-[[noreturn]] void shellLoop(){
-    while (true){
-        const std::string currentDirectory = "/";
+int shellLoop() {
+    while (true) {
         std::string inputBuffer;
-        printWelcomeMessage(currentDirectory, false);
+        printWelcomeMessage(false);
         std::getline(std::cin, inputBuffer);
         std::vector<std::string> words = split(inputBuffer); //split input string by space and tab
-        for (const auto& word: words) printf("%s\n", word.c_str()); //print parsed data
+        for (const auto &word: words) printf("%s\n", word.c_str()); //print parsed data
+        return 0;
     }
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
     shellLoop();
     return 0;
 }
